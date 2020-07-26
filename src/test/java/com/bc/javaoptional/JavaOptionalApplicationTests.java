a4ac8980df1d5ca456c55c3121b0d6052b05a411
Bruno <bruno.cesar@cilia.com.br>
2020-07-26 13:49:33 -0300

    Optional .of

--- src/test/java/com/bc/javaoptional/JavaOptionalApplicationTests.java
+++ src/test/java/com/bc/javaoptional/JavaOptionalApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class JavaOptionalApplicationTests {
@@ -14,5 +15,12 @@ class JavaOptionalApplicationTests {
     public void whenCreatesEmptyOption_thenCorrect() {
         Optional<String> empty = Optional.empty();
         assertFalse(empty.isPresent());
+    }
+
+    @Test
+    public void givenNonNull_whenCreatesNonNullable_thenCorrect() {
+        String name = "Test";
+        Optional<String> opt = Optional.of(name);
+        assertTrue(opt.isPresent());
     }
 }
