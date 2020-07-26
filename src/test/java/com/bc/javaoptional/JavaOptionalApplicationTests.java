557117f166404cd01930044c520417593784dc70
Bruno <bruno.cesar@cilia.com.br>
2020-07-26 15:23:40 -0300

    Optional map, example of optional filter with map

--- src/test/java/com/bc/javaoptional/JavaOptionalApplicationTests.java
+++ src/test/java/com/bc/javaoptional/JavaOptionalApplicationTests.java
@@ -3,6 +3,8 @@ package com.bc.javaoptional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -149,6 +151,44 @@ class JavaOptionalApplicationTests {
         assertFalse(priceIsInRange2(new Modem(null)));
         assertFalse(priceIsInRange2(new Modem(15.5)));
         assertFalse(priceIsInRange2(null));
+    }
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect() {
+        List<String> companyNames = Arrays.asList(
+                "paypal", "oracle", "", "microsoft", "", "apple");
+        Optional<List<String>> listOptional = Optional.of(companyNames);
+
+        int size = listOptional
+                .map(List::size)
+                .orElse(0);
+        assertEquals(6, size);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorks_thenCorrect2() {
+        String name = "Testing";
+        Optional<String> nameOptional = Optional.of(name);
+
+        int len = nameOptional
+                .map(String::length)
+                .orElse(0);
+        assertEquals(7, len);
+    }
+
+    @Test
+    public void givenOptional_whenMapWorksWithFilter_thenCorrect() {
+        String password = " password ";
+        Optional<String> passOpt = Optional.of(password);
+        boolean correctPassword = passOpt.filter(
+                pass -> pass.equals("password")).isPresent();
+        assertFalse(correctPassword);
+
+        correctPassword = passOpt
+                .map(String::trim)
+                .filter(pass -> pass.equals("password"))
+                .isPresent();
+        assertTrue(correctPassword);
     }
 
     public String getMyDefault() {
